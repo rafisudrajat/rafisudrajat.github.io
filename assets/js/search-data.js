9f520ecb5364e327edaebefd74f093fd21e36002
rafisudrajat <rafisudrajat.99@gmail.com>
2025-07-02 00:12:34 +0000

    Deploying to gh-pages from @ rafisudrajat/rafisudrajat.github.io@16d6e97e6ca9b644c77098c882aac5211ce4fb66 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-research",
           title: "research",
-          description: "A growing collection of your cool projects.",
+          description: "We focus on the design and development of algorithms to enable robust decision theory becomes practical computational tools for robotics and related domain. Such computational tools will enable robots to design their own strategies, such as deciding what data to use, how to gather the data, and how to adaptively improve its strategies, so as to accomplish various tasks well, despite various modelling errors and types of uncertainty, and despite limited to no information about the system and its operating environment.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/research/";
@@ -483,26 +483,51 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
+            },},{id: "projects-pomdp-planning-for-problems-with-complex-dynamics",
+          title: 'POMDP Planning for Problems with Complex Dynamics',
+          description: "Many POMDP (Partially Observable Markov Decision Process) solvers that can compute good approximate solutions on-line have been proposed. Some of them can even find good solutions in near real-time for several realistic problems. In this project, we aim to make on-line POMDP solvers more tractable for systems with complex non-linear dynamics.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project_rdl/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
+            },},{id: "projects-pomdp-planning-for-problems-with-large-discrete-action-space",
+          title: 'POMDP Planning for Problems with Large Discrete Action Space',
+          description: "Many good approximate POMDP (Partially Observable Markov Decision Process) solvers have been proposed since mid 2000, allowing POMDP to generate good strategies for realistic problems. Despite these advances, solving problems with large discrete action spaces remains difficult. This project aims to reduce such difficulties.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project_rdl/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
+            },},{id: "projects-on-line-pomdp-planning-toolkit-oppt",
+          title: 'On-line POMDP Planning Toolkit (OPPT)',
+          description: "In this project, we develop On-line POMDP Planning Toolkit (OPPT) that allows users to specify POMDP model of motion planing problems via a configuration file and 3D models of the robot and environment.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project_rdl/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-partially-observed-inventory-control",
+          title: 'Partially Observed Inventory Control',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project_rdl/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
+            },},{id: "projects-incorporating-defender-39-s-behaviour-in-autonomous-pen-testing",
+          title: 'Incorporating Defender&amp;#39;s Behaviour in Autonomous Pen-Testing',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project_rdl/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
